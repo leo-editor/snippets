b1c0d2c0a78e7b0f70e1b68f0260c72d505f4221
tbnorth <terry_n_brown@yahoo.com>
2013-08-29 10:30:08 -0500

    whoops, pasted wrong code in, this is previous again

--- git_node_versioning.py
+++ git_node_versioning.py
@@ -1,44 +1,78 @@
-import time
-import base64
-import textwrap
+from leo.extensions import sh
+from leo.extensions.sh import git
+import binascii
 
-p = c.p
-if not p.h.startswith('@bk '):
-    nd = g.findNodeAnywhere(c, '@backup-nodes')
-    if nd:
-        nd = nd.insertAsNthChild(0)
+GIT_DIR = "/home/tbrown/r/gittest/.git"
+
+git = git.bake(git_dir=GIT_DIR)
+
+hooks = 'select2', 'unselect2', 'save1', 'end1', 'after-create-leo-frame'
+
+if hasattr(c, 'node_change'):
+    for i in hooks:
+        try:
+            g.unregisterHandler(i, c.node_change)
+        except TypeError:
+            pass  # happens if it wasn't registered
+
+def node_change(tag, kwargs):
+
+    if tag in ('select2', 'after-create-leo-frame'):
+        v = kwargs['c'].p.v
+        v._init_content = (v.h, v.b)
+        return
+        
+    if tag == 'end1':
+        p = c.p
+    elif tag == 'save1':
+        p = kwargs['p']
+    elif tag == 'unselect2':
+        p = kwargs['old_p']
     else:
-        nd = p.insertAfter()
-    ts = time.strftime("%Y-%m-%d %H:%M:%S")
-    nd.h = "@bk %s %s" % (p.h, ts)
-    s = '\n'.join(textwrap.wrap(
-        base64.b64encode(p.b),
-        break_long_words=True
-    ))
-    nd.b = "# backup %s - %s\n# %s\n\n%s" % (
-        ts, p.h, p.get_UNL(with_proto=True), s)
-    g.es("%s - backed up"%ts)
-    c.redraw()
-    c.bodyWantsFocusNow()
-    
-else:
-    
-    hdr, s = p.b.split('\n\n', 1)
-    if hdr.startswith("# backup"):  # decode
-        hdr = hdr.replace('backup', 'BACKUP')
-        p.b = "%s\n\n%s" % (
-            hdr,
-            base64.b64decode(s.strip())
-        )
+        raise Exception("Unknown hook")
         
-    else:  # encode again
-        hdr = hdr.replace('BACKUP', 'backup')
-        s = '\n'.join(textwrap.wrap(
-            base64.b64encode(s.strip()),
-            break_long_words=True
-        ))
-        p.b = "%s\n\n%s" % (hdr, s)
+    v = p.v
+    
+    if not hasattr(v, '_init_content'):
+        return
         
-    c.redraw()
-    c.frame.body.setInsertPoint(0)
-    c.bodyWantsFocusNow()
+    if v._init_content != (v.h, v.b):
+
+        node_data = "%s\n%s" % (v.h, v.b)
+        
+        # get previous HEAD
+        try:
+            head = git('rev-parse', 'HEAD').strip()
+        except sh.ErrorReturnCode:
+            head = None
+        
+        # get hash for data, and save data in db
+        obj = git('hash-object', '-w', '--stdin', _in=node_data).strip()
+                     
+        # add path using gnx as a path
+        git('update-index', '--add', '--cacheinfo', '100755', obj, v.gnx)
+
+        # add path using UNL as a path
+        # have to use hex to make path safe for git
+        safe_path = binascii.b2a_hex(p.get_UNL(with_file=False))
+        git('update-index', '--add', '--cacheinfo', '100755', obj, safe_path)
+
+        # make a tree object from index        
+        tree = git("write-tree").strip()
+        
+        # commit tree object
+        cmd = ['commit-tree', tree]
+        if head:
+            cmd += ['-p', head]
+        commit = git(cmd, _in=p.get_UNL()).strip()
+
+        # update HEAD
+        git('reset', commit)  
+        
+        # in case we were triggered by save1 or a explicit commit command  
+        v._init_content = (v.h, v.b)
+
+c.node_change = node_change
+for i in hooks:
+    g.registerHandler(i, c.node_change)
+c.node_change('select2', dict(c=c, new_p=c.p, old_p=c.p, new_v=c.p.v, old_v=c.p.v))
